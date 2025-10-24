aa10a81f50ce4a3bcd94032468ecc5fc9318ad95
GroteGitJeppo <j.m.spuesens@tilburguniversity.edu>
2025-10-24 17:35:11 +0200

    lr model implemented

--- final_files/prediction-api/house_price_predictor.py
+++ final_files/prediction-api/house_price_predictor.py
@@ -20,11 +20,11 @@ class HousePricePredictor:
         if self.model is None:
             try:
                 model_repo = os.environ['MODEL_REPO']
-                file_path = os.path.join(model_repo, "xgboost_model.pkl")
+                file_path = os.path.join(model_repo, "lr_model.pkl")
                 self.model = pickle.load(open(file_path, 'rb'))
             except KeyError:
                 print("MODEL_REPO is undefined")
-                self.model = pickle.load(open("xgboost_model.pkl", 'rb'))
+                self.model = pickle.load(open("lr_model.pkl", 'rb'))
 
         df = pd.read_json(StringIO(json.dumps(prediction_input)), orient='records')
         xNew = df[['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 'condition', 'sqft_above', 'sqft_basement']]
