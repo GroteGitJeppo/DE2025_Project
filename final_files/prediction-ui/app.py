920fa650a720d30da472266e29b767e96c4d6f1f
GroteGitJeppo <j.m.spuesens@tilburguniversity.edu>
2025-10-25 22:43:33 +0200

    more debugging in the predictor ui

--- final_files/prediction-ui/app.py
+++ final_files/prediction-ui/app.py
@@ -1,24 +1,21 @@
 # importing Flask and other modules
-import json
-import os
-import logging
 import requests
+from requests.exceptions import JSONDecodeError, RequestException
 from flask import Flask, request, render_template, jsonify
+import os, logging, json
 
-# Flask constructor
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = app.logger
 
-
-# A decorator used to tell the application
-# which URL is associated function
 @app.route('/predicthouse', methods=["GET", "POST"])
 def predict_house_price():
     if request.method == "GET":
-        return render_template("input_form_page.html")
+        return render_template("input_form_page.html")  # always returns
 
-    elif request.method == "POST":
-        prediction_input = [
-            {
+    # POST:
+    try:
+        prediction_input = [{
             "bedrooms": float(request.form.get("bedrooms")),
             "bathrooms": float(request.form.get("bathrooms")),
             "sqft_living": int(float(request.form.get("sqft_living"))),
@@ -29,31 +26,40 @@ def predict_house_price():
             "condition": int(request.form.get("condition")),
             "sqft_above": int(float(request.form.get("sqft_above"))),
             "sqft_basement": int(float(request.form.get("sqft_basement")))
-            }
-        ]
+        }]
+    except Exception:
+        logger.exception("Invalid form input")
+        return render_template("response_page.html", prediction_variable="Invalid input"), 400
 
-        app.logger.debug("Prediction input : %s", prediction_input)
+    predictor_api_url = os.environ.get('PREDICTOR_API')
+    if not predictor_api_url:
+        logger.error("PREDICTOR_API environment variable is not set")
+        return render_template("response_page.html", prediction_variable="Server not configured (PREDICTOR_API)."), 500
 
-        # use requests library to execute the prediction service API by sending an HTTP POST request
-        # use an environment variable to find the value of the diabetes prediction API
-        # json.dumps() function will convert a subset of Python objects into a json string.
-        # json.loads() method can be used to parse a valid JSON string and convert it into a Python Dictionary.
-        predictor_api_url = os.environ['PREDICTOR_API']
-        res = requests.post(predictor_api_url, json=prediction_input, timeout=15)
+    url = predictor_api_url.rstrip('/')  # normalize /predicthouse
+
+    try:
+        res = requests.post(url, json=prediction_input, timeout=15)
         res.raise_for_status()
         try:
             payload = res.json()
-        except requests.exceptions.JSONDecodeError:
-            app.logger.error("Non-JSON response from predictor: %s", res.text[:200])
+        except JSONDecodeError:
+            logger.error("Non-JSON response from predictor: %s", res.text[:200])
             return render_template("response_page.html", prediction_variable="Invalid response from predictor."), 502
 
+        if isinstance(payload, dict) and 'result' in payload:
+            prediction_value = payload['result']
+            logger.info("Prediction Output : %s", prediction_value)
+            return render_template("response_page.html", prediction_variable=float(prediction_value))
 
-    else:
-        return jsonify(message="Method Not Allowed"), 405  # The 405 Method Not Allowed should be used to indicate
-    # that our app that does not allow the users to perform any other HTTP method (e.g., PUT and  DELETE) for
-    # '/checkdiabetes' path
+        logger.error("Unexpected JSON shape: %s", payload)
+        return render_template("response_page.html", prediction_variable="Unexpected predictor response."), 502
 
-
+    except RequestException:
+        logger.exception("Error calling predictor API")
+        return render_template("response_page.html", prediction_variable="Could not reach predictor."), 502
+    
+    
 # The code within this conditional block will only run the python file is executed as a
 # script. See https://realpython.com/if-name-main-python/
 if __name__ == '__main__':
