ff452d6f78c77a7b312efbe7ee7f157d5dd1d680
GroteGitJeppo <j.m.spuesens@tilburguniversity.edu>
2025-10-29 13:08:27 +0100

    added separate xgboost api

---
+++ final_files/prediction-api-xgboost/app.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask, request, jsonify
+
+from house_price_predictor import HousePricePredictor
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+dp = HousePricePredictor()
+@app.route('/predicthouse', methods=['POST']) # path of the endpoint. Except only HTTP POST request
+def predict_str():
+    prediction_input = request.get_json()
+    try:
+        result = dp.predict_single_record(prediction_input)
+        return jsonify({"result": result})
+    except Exception as e:
+        app.logger.exception("Prediction failed")
+        return jsonify({"error": str(e)}), 400
+
+
+
+# The code within this conditional block will only run the python file is executed as a
+# script. See https://realpython.com/if-name-main-python/
+if __name__ == '__main__':
+    app.run(port=int(os.getenv("PORT", 5000)), host='0.0.0.0', debug=True)
+
---
+++ final_files/prediction-api-xgboost/house_price_predictor.py
@@ -0,0 +1,33 @@
+import json
+import logging
+import os
+import pickle
+from io import StringIO
+
+import pandas as pd
+from flask import jsonify
+
+
+class HousePricePredictor:
+    def __init__(self):
+        self.model = None
+
+    def load_model(self, file_path):
+        self.model = pickle.load(open(file_path, 'rb'))
+
+    def predict_single_record(self, prediction_input):
+        logging.debug(prediction_input)
+        if self.model is None:
+            try:
+                model_repo = os.environ['MODEL_REPO']
+                file_path = os.path.join(model_repo, "xgboost_model.pkl")
+                self.model = pickle.load(open(file_path, 'rb'))
+            except KeyError:
+                print("MODEL_REPO is undefined")
+                self.model = pickle.load(open("xgboost_model.pkl", 'rb'))
+
+        df = pd.read_json(StringIO(json.dumps(prediction_input)), orient='records')
+        xNew = df[['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 'condition', 'sqft_above', 'sqft_basement']]
+        y_pred = self.model.predict(xNew)
+        house_value = float(y_pred[0]) 
+        return house_value
